e0fc60e0fba59e2c9b4c99bb788db20287ab8698
8UK0W5K1 <jeremie.deldem@gmail.com>
2023-07-27 17:57:38 +0200

    connect app to DB w/ .env credentials

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,3 +1,4 @@
+
 const PORT = 4000;
 const express = require('express');
 const app = express();
@@ -5,17 +6,62 @@ const app = express();
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+
+// Dotenv credentials for DB
+require("dotenv").config();
+
+
+
+const routes = express.Router()
+
+app.use("/api", routes)
+
 // body-parser
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+routes.use(bodyParser.urlencoded({extended: false}))
+routes.use(bodyParser.json())
 
 //cors
-app.use(cors())
+routes.use(cors())
 
 
-app.get('/', (req, res) => {
+//ROUTES API RESTFUL
+
+routes.get('/', (req, res) => {
   res.send('Hello World!')
 })
+routes.get('/products', (req, res) => {
+  res.send('Liste des produits')
+})
+
+
+// MongoDB Client 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PWD}@${process.env.DB_CLUSTER}.eyxi3nu.mongodb.net/?retryWrites=true&w=majority`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 
 app.listen(PORT, () => {
