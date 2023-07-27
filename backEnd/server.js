e7d5b65cbd492e18e3f3d3d5b4f4fa96d6a9ca7c
8UK0W5K1 <jeremie.deldem@gmail.com>
2023-07-27 13:24:08 +0200

    ajout de nodemon dans mon script

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,6 +2,6 @@ const PORT = 4000;
 const express = require('express');
 const app = express();
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Server is up and running on http://localhost:${PORT}`)
 })
