209ff19ebc20aa980a3152ba4cc2490822cfe60f
8UK0W5K1 <jeremie.deldem@gmail.com>
2023-07-27 14:06:48 +0200

    add body-parser & cors + '/' test route to my server

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,6 +2,22 @@ const PORT = 4000;
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// body-parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//cors
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
 app.listen(PORT, () => {
   console.log(`Server is up and running on http://localhost:${PORT}`)
 })
